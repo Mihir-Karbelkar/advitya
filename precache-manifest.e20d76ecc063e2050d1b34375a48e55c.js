09d54184418ab2fa8d64564b618bade2b5f5861f
mihir(faux__) <karbelkar.mihir@gmail.com>
2019-06-03 19:54:45 +0530

    Updates

---
+++ precache-manifest.e20d76ecc063e2050d1b34375a48e55c.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "1efff19d039eb9c4771318adf24b0ffd",
+    "url": "/advitya/static/media/Optimized-Logo.1efff19d.png"
+  },
+  {
+    "revision": "ff76dbabfc6a640186a5",
+    "url": "/advitya/static/css/main.6f8b2602.chunk.css"
+  },
+  {
+    "revision": "884a3539827bdb489627",
+    "url": "/advitya/static/js/runtime~main.884a3539.js"
+  },
+  {
+    "revision": "c9b012982f42af934a24",
+    "url": "/advitya/static/js/2.c9b01298.chunk.js"
+  },
+  {
+    "revision": "99e68c597881324ef8ea74f296b6d883",
+    "url": "/advitya/static/media/gear1.99e68c59.svg"
+  },
+  {
+    "revision": "c2cfdb6fcd1c6ffe74e678813dabe166",
+    "url": "/advitya/static/media/gear2.c2cfdb6f.svg"
+  },
+  {
+    "revision": "2ba3fdf091604fb98e6bda1ac7b7b79a",
+    "url": "/advitya/static/media/gear3.2ba3fdf0.svg"
+  },
+  {
+    "revision": "3caae7a689c6dca671280a7c61de4e18",
+    "url": "/advitya/static/media/gear4.3caae7a6.svg"
+  },
+  {
+    "revision": "62b631d42b9a3bcdc47abd0ca94051ce",
+    "url": "/advitya/static/media/petal.62b631d4.svg"
+  },
+  {
+    "revision": "ff76dbabfc6a640186a5",
+    "url": "/advitya/static/js/main.ff76dbab.chunk.js"
+  },
+  {
+    "revision": "59601f7882a52df71f5f676320146f62",
+    "url": "/advitya/static/media/trophy.59601f78.svg"
+  },
+  {
+    "revision": "9a27415caf7ac927d65d83e89060af77",
+    "url": "/advitya/static/media/trophy_m.9a27415c.svg"
+  },
+  {
+    "revision": "c081b1c3b44f961b279eec890eb50448",
+    "url": "/advitya/static/media/medal.c081b1c3.svg"
+  },
+  {
+    "revision": "e971b853126994eb3f7a3a24b58b4e2d",
+    "url": "/advitya/static/media/medal_m.e971b853.svg"
+  },
+  {
+    "revision": "e9d0b12bcbdf53cd1f2ee7e59d6d54ef",
+    "url": "/advitya/static/media/Hand.e9d0b12b.svg"
+  },
+  {
+    "revision": "4691d16a40c99844b10d96ec8729412f",
+    "url": "/advitya/static/media/ham.4691d16a.svg"
+  },
+  {
+    "revision": "20f79f149f5cfa707e44952a631a99ed",
+    "url": "/advitya/static/media/cross.20f79f14.svg"
+  },
+  {
+    "revision": "a798addb1e00a0ee9b51e0b9ec24fa82",
+    "url": "/advitya/index.html"
+  }
+];
